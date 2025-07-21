acbc84278a8b198b732bfddc3706e750501550f7
Parth Khatri <31758560+12627550@users.noreply.github.com>
2025-07-21 22:29:21 +1000

    Update ContactWindow.js

--- src/components/ContactWindow.js
+++ src/components/ContactWindow.js
@@ -5,9 +5,9 @@ const ContactWindow = () => (
   <div className="bg-white dark:bg-slate-800 p-4 rounded-xl shadow-xl w-72">
     <h2 className="text-xl font-bold mb-2">📞 Contact</h2>
     <p className="text-sm">
-      📧 Email: yourname@example.com<br />
-      🔗 GitHub: github.com/yourprofile<br />
-      🔗 LinkedIn: linkedin.com/in/yourprofile
+      📧 Email: pknino01@gmail.com<br />
+      🔗 GitHub: github.com/12627550<br />
+      🔗 LinkedIn: linkedin.com/in/parth-khatri-01
     </p>
   </div>
 );
